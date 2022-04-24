15302bd79ce4d0ae9182e2c6b2e628734de2087b
望月祐汰 <mczk9402@gmail.com>
2022-04-24 14:40:49 +0900

    50ページ目

--- src/main/kotlin/example.kt
+++ src/main/kotlin/example.kt
@@ -111,14 +111,197 @@ class GreeterImpl: Greeter{
     }
 }
 
+// if文を式にする
+fun printOddOrEvenNumberText(num: Int) {
+    var text = ""
+    if(num % 2 == 1) {
+        text = "奇数"
+    } else {
+        text = "偶数"
+    }
+
+    println(text);
+}
+
+fun printOddOrEvenNumberText2(num: Int) {
+    val text = if (num % 2 == 1) {
+        "奇数"
+    } else {
+        "偶数"
+    }
+
+    println(text)
+}
+
+fun printOddOrEventNumberText3(num: Int) {
+    val text = if(num % 2 == 1) "奇数" else "偶数"
+    println(text)
+}
+
+fun printOddOrEvenNumberText4(num: Int) {
+    val text = when(num) {
+        100 -> {
+            "Equal to 100"
+        }
+        200 -> {
+            "Equal to 200"
+        }
+        else -> {
+            "Undefined value"
+        }
+    }
+
+    println(text)
+}
+
+//class User1 {
+//    var name: String = ""
+//
+//    fun getName(): String {
+//        return this.name
+//    }
+//
+//    fun setName(name: String) {
+//        this.name = name
+//    }
+//}
+//
+//class User2(id: Int) {
+//    val id: Int = id
+//    // valで定義したものは後から変更できない
+//    var name: String = ""
+//}
+//
+//class User3 {
+//    // 遅延初期化tら　後から書き換える varのみ
+//    lateinit var name: String
+//}
+
+//class User4 {
+//    lateinit var name: String
+//    val isValidName: Boolean
+//    get() = name != ""
+//}
+
+//class User5 {
+//    var name: String = ""
+//    var hoge: String = ""
+//
+//    set(value) {
+//        if (value == "") {
+//            field = "Kotlin"
+//        } else {
+//            field = value
+//        }
+//    }
+//
+//    val isValidName: Boolean
+//    get() = hoge != ""
+//}
+
+class User6 {
+    val id: Int = 1
+    val name = "Kotlin"
+
+    override fun equals(other: Any?): Boolean {
+        if(this === other) return true
+        if(javaClass != other?.javaClass) return false
+
+        other as User6
+
+        if(id != other.id) return false
+        if(name != other.name) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return 31 * name.hashCode() + id
+    }
+
+    override fun toString(): String {
+        return "User6(id=$id,name=$name)"
+    }
+}
+
+
+
 fun main() {
+
+    data class User9(val id: Int, val name: String = "default Name", val age: Int)
+
+
+    val user = User9(id = 1, age = 30)
+    println(user.toString())
+
+//    data class User8(val id: Int, val name:String = "Default Name")
+//
+//    val user = User8(1)
+//    println(user.toString())
+
+//    fun printUserInfo (id: Int, name: String = "Default Name") {
+//        println("id=$id name=$name")
+//    }
+//
+//    printUserInfo(1,"mochizuki")
+
+//    data class User7(val id: Int, var name: String) {
+//        val isValidName: Boolean
+//        get() = name != ""
+//    }
+//
+//    val user = User7(1, "mochizuki")
+//    // toString クラス名(プロパティ名=値,...)
+//    println(user.toString())
+//    // componentN  順番を指定してプロパティの値を取得する
+//    println(user.component2())
+//    // copy関数
+//    val update = user.copy(1, "kotlin")
+//    println(update)
+
+//    val user = User7(1, "mochizuki")
+//    val same = User7(1, "mochizuki")
+//    val other = User7(2, "mochizuki")
+//    println("user=${user.hashCode()}")
+//    println("same=${same.hashCode()}")
+//    println("other=${other.hashCode()}")
+//    val set = hashSetOf(user)
+//    println(set.contains(same))
+//    println(user == same)
+
+//    val userA = User6()
+//    val userB = User6()
+
+//    println(userA == userB)
+//    println(userB)
+//    var user = User5()
+//    user.hoge = ""
+//    println(user.hoge)
+//    user.hoge = "mochizuki"
+//    println(user.hoge)
+//    println(user.isValidName)
+//    val user = User4()
+//    user.name = "mochizuki"
+//    println(user.isValidName)
+//    val user = user3()
+//    println(user.name)
+//    val user = User2(1)
+//    user.name = "mochizuki"
+//    user.id = 2
+//    val user = User1()
+//    user.name = "mochizuki"
+//    println(user.name)
+// printOddOrEvenNumberText(2)
+//    printOddOrEvenNumberText2(2)
+//    printOddOrEventNumberText3(1)
+//    printOddOrEvenNumberText4(300)
 // コレクション
 // Set
 //    val set: Set<String> = setOf("one","two","three")
 //    println(set.contains("for"))
-    val mutableSet:MutableSet<String> = mutableSetOf("one","two","three")
-    mutableSet.add("for")
-    println(mutableSet)
+//    val mutableSet:MutableSet<String> = mutableSetOf("one","two","three")
+//    mutableSet.add("for")
+//    println(mutableSet)
 
 // Map
 // デフォルトはimmutable
